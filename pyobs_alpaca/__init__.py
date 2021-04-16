cc0dbc9a3b721c4647065de863eb3d8191d01b90
Tim-Oliver Husser <thusser@uni-goettingen.de>
2021-04-16 11:13:23 +0200

    updated docstrings

--- pyobs_alpaca/__init__.py
+++ pyobs_alpaca/__init__.py
@@ -1,3 +1,10 @@
+"""
+ASCOM Alpaca modules (pyobs_alpaca)
+-----------------------------------
+
+TODO: write doc
+"""
+
 from .telescope import AlpacaTelescope
 from .focuser import AlpacaFocuser
 from .dome import AlpacaDome
